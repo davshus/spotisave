2301e3a73c83bc88ea3e6443bd213228df91205c
David Shustin <david.shustin@gmail.com>
2017-07-19 13:00:43 -0400

    Complete everything except for state checking

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
 var hash = require('./hash')();
 var apigen = require('./api');
-
+var status = require('./status');
+var $ = require('jquery');
 var auth = hash.obj;
 
 // TODO: ADD STATE CHECKING
-
+function error(msg) {
+  status.stop();
+  console.error(msg);
+  status._elem.innerHTML = msg;
+  $('#dots').css('display', 'none');
+  $('.done-background').css('fill', 'rgb(223, 1, 1)').css('opacity', 1);
+  $('#done-container').css('display', 'flex');
+  $('.error1').addClass('draw');
+  $('.error2').addClass('draw');
+}
+function success(msg) {
+  status.stop();
+  console.log(msg);
+  status._elem.innerHTML = msg;
+  $('#dots').css('display', 'none');
+  $('.done-background').css('opacity', 1);
+  $('#done-container').css('display', 'flex');
+  $('.checkmark').addClass('draw');
+}
 var api = apigen(auth.access_token);
 function generateDWName() {
   var date = new Date();
@@ -14,18 +33,52 @@ function generateDWName() {
   return playlistName;
 }
 async function exportDW() {
-  var userInfo = await api.myInfo();
+  status.start();
+  status.setStatus('Making an awesome playlist...');
+  try {
+    var userInfo = await api.myInfo();
+  } catch (e) {
+    console.error(e);
+    return "Could not retrieve user info. Try going home and signing in again.";
+  }
   var uid = userInfo.id;
-  var playlists = await api.myPlaylists();
+  try {
+    var playlists = await api.myPlaylists();
+  } catch (e) {
+    console.error(e);
+    return "Could not retrieve playlists. Try going home and signing in again.";
+  }
   var dw = playlists.items.filter(i=>i.owner.id == "spotify" && i.name == "Discover Weekly");
   if (dw.length < 1) {
-    console.error("No Discover Weekly was found!");
-    return false;
+    console.error(e);
+    return "No Discover Weekly was found. Try unfollowing it, then following it.";
   }
   dw = dw[0];
-  var dwTracks = await api.playlistTracks(dw.href);
+  try {
+    var dwTracks = await api.playlistTracks(dw.href);
+  } catch (e) {
+    console.error(e);
+    return "Could not get Discover Weekly's tracks.";
+  }
   var name = generateDWName();
-  var createPlaylistRes = await api.createPlaylist(name, uid);
-  var addPlaylistRes = await api.addTracksToPlaylist(createPlaylistRes.href, dwTracks.items.map(i=>i.track.uri));
+  try {
+    var createPlaylistRes = await api.createPlaylist(name, uid);
+  } catch (e) {
+    console.error(e);
+    return "Could not create a new playlist to back up to.";
+  }
+  try {
+    await api.addTracksToPlaylist(createPlaylistRes.href, dwTracks.items.map(i=>i.track.uri));
+  } catch (e) {
+    console.error(e);
+    return "Could not add tracks to a new playlist.";
+  }
+  return true;
 }
-// exportDW();
+exportDW().then(res => {
+  if (res === true) {
+    success("Done!");
+  } else {
+    error(res);
+  }
+});
---
+++ src/status.js
@@ -0,0 +1,21 @@
+// PLEASE DON'T ADD PERIODS TO THE STATUS
+// PLEASE
+module.exports = (function() {
+  var obj = new Object();
+  obj._status = "Loading";
+  obj._counter = 0;
+  obj._elem = document.getElementById('status');
+  obj._statGen = () => {
+    obj._elem.innerHTML = `${obj._status}${'.'.repeat(obj._counter)}`
+    obj._counter++;
+    obj._counter = obj._counter % 4;
+  };
+  obj.setStatus = stat => {
+    var tmp = stat;
+    if (stat.substring(stat.length - 3) == '...') tmp = stat.substring(0, stat.length - 3)
+    obj._status = tmp;
+  };
+  obj.start = () => obj._interval = setInterval(obj._statGen, 333);
+  obj.stop = () => clearInterval(obj._interval);
+  return obj;
+})();
